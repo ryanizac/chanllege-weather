7c1212d37976c4e5fbeb689c6f13b2bc93f73a0c
Ryan Izac <io.ryan@outlook.com>
2022-06-04 22:25:42 -0300

    import my icons

---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+import BackSvg from '@assets/icon/back.svg';
+import HeartSvg from '@assets/icon/heart.svg';
+import MenuSvg from '@assets/icon/menu.svg';
+import XSvg from '@assets/icon/x.svg';
+
+export { BackSvg, HeartSvg, MenuSvg, XSvg };
